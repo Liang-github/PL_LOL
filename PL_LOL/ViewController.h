fe34890fb86dd3cd88b2edc35efc50053aefd8dc
LIANG <pl941227@icloud.com>
2017-08-17 13:09:20 +0800

    Initial Commit

---
+++ PL_LOL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PL_LOL
+//
+//  Created by PengLiang on 2017/8/17.
+//  Copyright © 2017年 PengLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PL_LOL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PL_LOL
+//
+//  Created by PengLiang on 2017/8/17.
+//  Copyright © 2017年 PengLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
